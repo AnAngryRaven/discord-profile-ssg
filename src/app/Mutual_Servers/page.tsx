8ee21906153f46b8875a10e4c05b384d161821f3
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-06-26 11:03:54 -0400

    mutual servers page quickly making this generic should make it easily portable to the mutual friends tab, too

--- src/app/Mutual_Servers/page.tsx
+++ src/app/Mutual_Servers/page.tsx
@@ -1,14 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { promises as fs } from 'fs';
 import styles from './mutual_servers.module.css';
 import { JSX } from 'react';
+import Link from 'next/link';
 
 export default async function MutualServers() {
-    const server_data = JSON.parse(await fs.readFile(process.cwd() + '/public/server_data.json', 'utf-8'))
+    const server_data: ServerEntry[] = JSON.parse(await fs.readFile(process.cwd() + '/public/server_data.json', 'utf-8'))
+
+    const data = server_data.map(entry => Category(entry))
 
     return (
-        <></>
+        <div className={["body-content", "padding-bottom", styles.box_height].join(" ")}>
+            {data}
+        </div>
     )
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-//interface ServerData {}
+
+//todo!("split off for the Mutual_Friends page");
+function Category(data: ServerEntry) {
+    const category_entries = data.entries.map(entry => ServerEntry(entry))
+    
+    if(data.category == "" || data.category == "none".toLowerCase()) {
+        return (
+            <div key="none-category">{category_entries}</div>
+            
+        )
+    }
+    else {
+        return (
+            <div key={data.category}>
+                <details open={data.open}>
+                    <summary className={styles.category_entry}>{data.category}</summary>
+                    {category_entries}
+                </details>
+            </div>
+        )
+    }
+}
+
+function ServerEntry(data: ServerDetails) {
+    return(
+        <Link className={styles.server_entry} key={data.name} href={data.link} >
+            <img className={styles.entry_img} src={data.img_path}/>
+            <p>{data.name}</p>
+        </Link>
+    )
+}
+
+interface ServerEntry {
+    category: string,
+    open: boolean,
+    entries: ServerDetails[],
+    img_path?: string
+}
+
+interface ServerDetails {
+    img_path: string,
+    name: string,
+    link: string
+}

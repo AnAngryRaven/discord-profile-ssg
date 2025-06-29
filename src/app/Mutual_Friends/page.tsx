51344cc2eeef59bae512ca735252a28492cf033d
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2025-06-29 08:09:16 -0400

    mutual friends page

--- src/app/Mutual_Friends/page.tsx
+++ src/app/Mutual_Friends/page.tsx
@@ -1,5 +1,16 @@
-export default function MutualFriends() {
+import { promises as fs } from 'fs';
+import styles from '../List/CustomList.module.css';
+import Category, { ServerEntry } from '../List/CustomList.tsx';
+
+
+export default async function MutualFriends() {
+    const server_data: ServerEntry[] = JSON.parse(await fs.readFile(process.cwd() + '/public/friend_data.json', 'utf-8'))
+
+    const data = server_data.map(entry => Category(entry))
+
     return (
-        <></>
+        <div className={["body-content", "padding-bottom", styles.box_height].join(" ")}>
+            {data}
+        </div>
     )
 }
